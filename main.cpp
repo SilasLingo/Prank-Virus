79674ea6c1b01297eaec8e83b0379ddae6bafb59
SilasLingo <90724942+SilasLingo@users.noreply.github.com>
2021-09-14 16:35:26 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main() {
     while (true) {
         std::cout << " LOL :D  ";
         SetCursorPos(900, 500);
-
+        SetCursorPos(910, 500);
+        SetCursorPos(920, 500);
+        SetCursorPos(930, 500);
+        SetCursorPos(940, 500);
+        SetCursorPos(950, 500);
+        
+        
     }
 
 }
